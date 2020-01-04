0e24d18385a95e88219f15bc430c99d233b9ae0b
nalberga <nicolas.alberga@gmail.com>
2020-01-04 14:11:29 -0500

    changes 3

--- Index.js
+++ Index.js
@@ -533,7 +533,7 @@ app.get('/login', function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('The server is Listening...')
 });
 
